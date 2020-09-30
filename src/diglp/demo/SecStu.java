691ed3989e10544b4fc488a5b835e8f6a1f275a1
lotdoney <doneycow@foamail.com>
2020-09-30 23:56:41 +0800

    test

--- src/diglp/demo/SecStu.java
+++ src/diglp/demo/SecStu.java
@@ -1,20 +1,35 @@
+//抽取随机数据数组的应用
+
 package diglp.demo;
 
-import java.io.Console;
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.ConsoleHandler;
 
-import static java.lang.Math.*;
-
-public class SecStu {
-
+public class SecStu{
     public static void main(String[] args){
-        int sum = 0;
-        int n = 1;
-        while (n<=100){
-            sum += n;
-            n++;
+        Scanner input = new Scanner(System.in);
+        System.out.println("我们将从100个数字中抽取幸运观众！");
+        int n = 100;
+        System.out.println("请问抽出几名幸运观众？");
+        int k = input.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i<n;i++){
+            numbers[i] = i+1;
         }
-        System.out.println(sum);
+
+        int[] result = new int[k];
+        for (int i =0;i<k;i++){
+            int r = (int)(Math.random()*n);
+            result[i] = numbers[r];
+            numbers[r] = numbers[n-1];
+            n--;
+        }
+
+        Arrays.sort(result);
+        System.out.println(Arrays.toString(result));
+
     }
+
 }
